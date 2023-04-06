3d22842fb1b530a3d29d9a5c00a9bcfdcfbee844
Rampawara99 <ramjadhaw1999@gmail.com>
2023-04-07 01:40:30 +0530

    updted

--- src/index.js
+++ src/index.js
@@ -19,11 +19,10 @@ const connection = require("./connector");
 app.get("/api/orders", (req, res) => {
   try {
     // Execute query to get the first 10 orders from the database
-    connection.query("select * from orders limit 10 offset 0", (err, result, next) => {
+    connection.query("select * from orders limit 10 offset 0", (err, result) => {
       if (err) {
         // Handle errors
         res.status(400).json({ message: "400 bad request" });
-        next();
       } else {
         // Send the results back to the client
         res.status(200).json(result);
@@ -41,11 +40,10 @@ app.get("/api/orders/:lim/:off", (req, res) => {
     // Extract limit and offset from the request parameters
     const { lim, off } = req.params;
     // Execute query to get data from limit and offset
-    connection.query(`select * from orders limit ${lim} offset ${off}`, (err, result, next) => {
+    connection.query(`select * from orders limit ${lim} offset ${off}`, (err, result) => {
       if (err) {
         // Handle errors
         res.status(400).json({ message: "400 bad request" });
-        next();
       } else {
         // Send the results back to the client
         res.status(200).json(result);
